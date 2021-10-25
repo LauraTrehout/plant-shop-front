3c730b36db87f67334e6c9534f5e1471430adca5
LauraTrehout <lauratrehout@gmail.com>
2021-10-25 19:05:43 +0200

    started project, header and cart icon

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { FaCartArrowDown } from 'react-icons/fa'
+import './Header.css'
+
+const Header = () => {
+    return ( 
+        <div className='header'>
+        <h1>The Plant Shop</h1>
+        <FaCartArrowDown className='cart'/>
+        </div>
+     );
+}
+ 
+export default Header;
